2c4137d22841385da6f0dd154caceb049354c92a
Tair Shechter <tair.shechter@overwolf.com>
2026-02-08 16:22:30 +0200

    refactor surface friction modifier to constructor

--- src/ball.py
+++ src/ball.py
@@ -6,12 +6,5 @@ class Ball:
         self.total_diff = diff  
         self.mass_bias = mass_bias
         self.grit = grit
-        self.cover_type = cover_type    
-
-    def get_surface_friction_modifier(self):
-        '''
-        Lower grit (rougher) = more friction earlier.
-        1500 grit is fairly aggressive compared to 4000 or Polished.
-        '''
-        # Baseline is 2000 grit = 1.0
-        return 2000 / self.grit
+        self.cover_type = cover_type
+        self.surfacte_friction_modifier = 2000 / self.grit
